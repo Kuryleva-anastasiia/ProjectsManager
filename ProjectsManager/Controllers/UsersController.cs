e12f7201484d6dcac213487915f9dd83cb6b3bde
Kuryleva Anastasia <knastya76@yandex.ru>
2025-05-26 14:10:52 +0300

    Создание отчетов

--- ProjectsManager/Controllers/UsersController.cs
+++ ProjectsManager/Controllers/UsersController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjectsManager.Models;
 using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Word;
 
 namespace ProjectsManager.Controllers
 {
@@ -17,12 +19,14 @@ namespace ProjectsManager.Controllers
         private readonly ProjectsManagerContext _context;
         private readonly INotyfService _toastNotification;
         private readonly ILogger<UsersController> _logger;
+        IWebHostEnvironment _appEnvironment;
 
-        public UsersController(ProjectsManagerContext context, INotyfService toastNotification, ILogger<UsersController> logger)
+        public UsersController(ProjectsManagerContext context, INotyfService toastNotification, ILogger<UsersController> logger, IWebHostEnvironment appEnvironment)
         {
             _context = context;
             _toastNotification = toastNotification;
             _logger = logger;
+            _appEnvironment = appEnvironment;
         }
 
         // GET: Users
@@ -287,6 +291,95 @@ namespace ProjectsManager.Controllers
         private bool UserExists(int id)
         {
             return _context.Users.Any(e => e.Id == id);
+        }
+
+        // GET: Users/Analize
+        public IActionResult Analize()
+        {
+            _toastNotification.Warning("\nОтчет создан в папке Отчеты!\n", 10);
+
+            var tasks = _context.Tasks.Include(t => t.AssignedUser).Include(t => t.Priority).Include(t => t.Project).Include(t => t.Status).Where(s => s.CompletionDate <= DateOnly.FromDateTime(DateTime.Now) && s.Status.Name == "Готова");
+
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            return View(tasks);
+        }
+
+        // POST: Users/Analize
+        [HttpPost, ActionName("Analize")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Analize(DateOnly start, DateOnly end, string file)
+        {
+
+
+            Microsoft.Office.Interop.Excel.Application winword = new Microsoft.Office.Interop.Excel.Application()
+            {
+                //Отобразить Excel
+                Visible = true,
+                //Количество листов в рабочей книге
+                SheetsInNewWorkbook = 1
+            };
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+
+            //Добавить рабочую книгу
+            Workbook workBook = app.Workbooks.Add(Type.Missing);
+
+            //Отключить отображение окон с сообщениями
+            app.DisplayAlerts = false;
+
+            //Получаем первый лист документа (счет начинается с 1)
+            Worksheet sheet = (Worksheet)app.Worksheets.get_Item(1);
+
+            //Название листа (вкладки снизу).
+            sheet.Name = string.Concat("Отчет ", start.ToString("dd.MM.yyyy"), " - ", end.ToString("dd.MM.yyyy"));
+
+            var tasks = _context.Tasks.Include(t => t.AssignedUser).Include(t => t.Priority).Include(t => t.Project).Include(t => t.Status).Where(s => s.CompletionDate <= end && s.CompletionDate >= start && s.Status.Name == "Готова");
+
+
+            sheet.Cells[1, 1] = string.Concat("Промежуток времени: ");
+            sheet.Cells[1, 2] = string.Concat(start.ToString("dd.MM.yyyy"), " - ", end.ToString("dd.MM.yyyy"));
+
+            //заполнение имен столбцов в excel
+
+            sheet.Cells[3, 1] = "Дата выполнения";
+            sheet.Cells[3, 2] = "Ответственный сотрудник";
+            sheet.Cells[3, 3] = "";
+            sheet.Cells[3, 4] = "";
+            sheet.Cells[3, 5] = "Название проекта";
+
+            int j = 4;
+
+            foreach (var task in tasks)
+            {
+
+                sheet.Cells[j, 1] = task.CompletionDate.ToString();
+                sheet.Cells[j, 2] = task.AssignedUser.LastName;
+                sheet.Cells[j, 3] = task.AssignedUser.Name;
+                sheet.Cells[j, 4] = task.AssignedUser.Patronymic;
+                sheet.Cells[j, 5] = task.Project.Name;
+                j++;
+            }
+
+            sheet.Cells[j, 5] = "Выполнено задач: " + Convert.ToString(j-1);
+
+            sheet.Columns.AutoFit();
+
+            // и места где его нужно сохранить*/
+            app.Application.ActiveWorkbook.SaveAs($"{_appEnvironment.WebRootPath}/Отчеты/{file}.xlsx", Type.Missing,
+              Type.Missing, Type.Missing, Type.Missing, Type.Missing, XlSaveAsAccessMode.xlNoChange,
+              Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            app.Quit();
+
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+
+            winword.Quit();
+
+            _toastNotification.Custom("Отчет создан в папке \"Отчеты\"!", 6, "#602AC3", "fa fa-user");
+            return RedirectToAction("Analize");
         }
     }
 }
